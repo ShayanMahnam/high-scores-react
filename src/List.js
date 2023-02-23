b7ac204669213bea3df70e92a5e6e2f971d8622d
Shayan Mahnam <shayan.mahnam.uk@gmail.com>
2023-02-23 22:56:56 +0000

    level 2 done

--- src/List.js
+++ src/List.js
@@ -1,17 +1,24 @@
- 
 
-const List = ({ data }) =>
-  data.map((element, index) => (
-    <div className="container" key={index}>
-      <h2 className="blue">HIGH SCORES: {element.name}</h2>
-      <ul className="list">
-        {element.scores.map((score, index) => (
-          <li className="items" key={index}>
-            <p className="black">{score.n.toUpperCase()}</p> <p className="red">{score.s}</p>
-          </li>
-        ))}
-      </ul>
-    </div>
-  ));
+const List = ({ data }) => {
+  const sortedData = [...data].sort((a, b) => a.name.localeCompare(b.name));
 
-export default List
+  return (
+    <>
+      {sortedData.map((element, index) => (
+        <div className="container" key={index}>
+          <h2 className="blue">HIGH SCORES: {element.name}</h2>
+          <ul className="list">
+            {element.scores.map((score, index) => (
+              <li className="items" key={index}>
+                <p className="black">{score.n.toUpperCase()}</p>{" "}
+                <p className="red">{score.s}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default List;
