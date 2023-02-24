142c367a13863bd6ae39e81827517d462fa530f5
Shayan Mahnam <shayan.mahnam.uk@gmail.com>
2023-02-24 00:03:00 +0000

    level 3 done

--- src/List.js
+++ src/List.js
@@ -8,12 +8,14 @@ const List = ({ data }) => {
         <div className="container" key={index}>
           <h2 className="blue">HIGH SCORES: {element.name}</h2>
           <ul className="list">
-            {element.scores.map((score, index) => (
-              <li className="items" key={index}>
-                <p className="black">{score.n.toUpperCase()}</p>{" "}
-                <p className="red">{score.s}</p>
-              </li>
-            ))}
+            {element.scores
+              .sort((a, b) => b.s - a.s)
+              .map((score, index) => (
+                <li className="items" key={index}>
+                  <p className="black">{score.n.toUpperCase()}</p>{" "}
+                  <p className="red">{score.s}</p>
+                </li>
+              ))}
           </ul>
         </div>
       ))}
